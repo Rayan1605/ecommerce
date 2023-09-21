3ebec794722ace1281f7d3f0ce4ecfe583881497
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2023-09-21 15:28:54 +0100

    End

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -9,6 +9,7 @@ const generateToken = (res, userId) => {
   res.cookie('jwt', token, {
     httpOnly: true,// This make it a Http only cookie which means it can only be accessed by the server and not by javascript
     secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production
+    //The reason is to set that in true you need to have a https connection but in production we don't have https connection
     sameSite: 'strict', // Prevent CSRF attacks
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
